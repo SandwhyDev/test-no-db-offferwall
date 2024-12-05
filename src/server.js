13ce3b6f8529e25fff113042c1cac3e39e17a838
sandwhydev <sandwhydev@gmail.com>
2024-12-05 12:18:12 +0700

    adadd

--- src/server.js
+++ src/server.js
@@ -10,11 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 //MIDDLEWARE
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
